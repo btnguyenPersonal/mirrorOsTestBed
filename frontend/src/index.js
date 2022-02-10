73192fc7729fbe328d1c38bb34bba559fae8dc56
Ghoulshine <btbaccam@iastate.edu>
2022-02-10 11:57:34 -0600

    Added web terminal placeholder to main page

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,10 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { TerminalContextProvider } from "react-terminal";
 
 ReactDOM.render(
   <React.StrictMode>
+  <TerminalContextProvider>
     <App />
+    </TerminalContextProvider>,
+      rootElement
   </React.StrictMode>,
   document.getElementById('root')
 );
