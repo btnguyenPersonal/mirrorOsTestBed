cbb2630c0118733c6aaab2faa21918bec393c772
Ghoulshine <btbaccam@iastate.edu>
2022-02-10 10:29:23 -0600

    re-acclimating to GitLab stuff

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>
-          Login Page
+          re-acclimating to GitLab stuff
         </p>
       </header>
     </div>
