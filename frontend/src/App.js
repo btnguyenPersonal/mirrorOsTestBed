8c26ee793164eda9ad839240655145dc916a11a9
Ghoulshine <btbaccam@iastate.edu>
2022-02-10 10:36:58 -0600

    making Brandon_branch

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,9 +4,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          re-acclimating to GitLab stuff
-        </p>
+        <p>re-acclimating to GitLab stuff</p>
+        <p>making Brandon_branch</p>
       </header>
     </div>
   );
