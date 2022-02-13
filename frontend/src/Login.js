8b80f69db25aba6f837bb69618eddc38609d4501
Christopher Spartz <cgspz.17@gmail.com>
2022-02-13 11:48:55 -0600

    Basic front end page done with no backend

---
+++ frontend/src/Login.js
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import "./Login.css";
+
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function validateForm() {
+    return email.length > 0 && password.length > 0;
+  }
+
+  function handleSubmit(event) {
+    sendCredentials(email,password);
+    event.preventDefault();
+  }
+
+  function sendCredentials(email,password)
+  {
+    var data = {
+        email: email,
+        password: password
+    };
+    console.log(JSON.stringify(data));
+    var xhr = new XMLHttpRequest();
+    var url = "localhost:8080/api/login";
+    xhr.open("POST",url,true);
+    xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
+    xhr.send(JSON.stringify(data));
+    xhr.onloadend = function () {
+        // done
+    };
+  }
+
+  return (
+    <div className="Login">
+      <Form onSubmit={handleSubmit}>
+        <Form.Group size="lg" controlId="email">
+          <Form.Label>Email: </Form.Label>
+          <Form.Control
+            autoFocus
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </Form.Group>
+        <Form.Group size="lg" controlId="password">
+          <Form.Label>Password: </Form.Label>
+          <Form.Control
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </Form.Group>
+        <Button block size="lg" type="submit" disabled={!validateForm()}>
+          Login
+        </Button>
+      </Form>
+    </div>
+  );
+}
