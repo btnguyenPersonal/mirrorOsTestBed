cbcfb9c42309b3074b8155e3c7f99d6469fde05d
btnguyen <btnguyen@iastate.edu>
2022-03-01 12:17:02 -0600

    yes.

--- backend/app/routes/api.routes.js
+++ backend/app/routes/api.routes.js
@@ -2,7 +2,7 @@ module.exports = (app) => {
   const api = require("../controllers/api.controller.js");
   var router = require("express").Router();
   router.post("/api/login", api.login);
-  router.post("/api/restart/:id", api.restartPort);
+  router.post("/api/reboot/:id", api.rebootPort);
   router.post("/api/useComputer/", api.useComputer);
   router.post("/api/releaseComputer/", api.releaseComputer);
   app.use("", router);
