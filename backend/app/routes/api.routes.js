424b0d0f26d75a412867883cd63d38970fc9106d
vprao <vprao@iastate.edu>
2022-03-22 17:04:18 -0500

    File upload backend

--- backend/app/routes/api.routes.js
+++ backend/app/routes/api.routes.js
@@ -1,8 +1,10 @@
 module.exports = (app) => {
   const api = require("../controllers/api.controller.js");
+  const fileHandler = require("../upload/fileHandler.js");
   var router = require("express").Router();
   router.post("/api/login", api.login);
   router.post("/api/reboot/:id", api.rebootPort);
+  router.post("/api/upload/:id", fileHandler.uploader, api.fileUpload);
   router.post("/api/useComputer/", api.useComputer);
   router.post("/api/releaseComputer/", api.releaseComputer);
   app.use("", router);
---
+++ backend/app/upload/fileHandler.js
@@ -0,0 +1,46 @@
+const multer = require('multer');
+const mkdirp = require('mkdirp');
+
+exports.uploader = (req, res, next) => {
+	var id = req.params.id;
+	
+	//create required directory
+	mkdirp.sync('./app/upload/files/' + id);
+	
+	//storage details - path and file name
+	var storage = multer.diskStorage({
+		destination: function(req, file, cb) {
+			cb(null, './app/upload/files/' + id + '/');
+		},
+		filename: function(req, file, cb) {
+			cb(null, 'kernel.img');
+		}
+	});
+	
+	//filters out wrong file types
+	const fileFilter = (req, file, cb) => {
+		if(file.mimetype === 'application/octet-stream')
+			cb(null, true);
+		else
+			cb(new Error, false);
+	};
+	
+	var upload = multer({
+		storage: storage, 
+		limits: {
+			fileSize: 30 * 1024 * 1024
+			},
+		fileFilter: fileFilter
+	}).single('imgFile');
+	
+	upload (req, res, (err) => {
+		if(!err)
+			next();
+		else if (err.code === 'LIMIT_FILE_SIZE')
+			return res.status(400).send({ message: 'File size too large' });
+		else if(err instanceof multer.MulterError)
+			return res.status(500).send({ message: 'File upload error' });
+		else
+			return res.status(400).send({ message: 'Unsupported file type' });
+	});
+};
