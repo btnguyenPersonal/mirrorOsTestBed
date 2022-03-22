f00702b7329d72c9181daee9c10ad9e42d4a82f6
Christopher Spartz <cgspz.17@gmail.com>
2022-03-22 16:29:47 -0500

    Simple GUI for uploading files.

--- frontend/src/Terminal.js
+++ frontend/src/Terminal.js
@@ -1,11 +1,56 @@
 import { ReactTerminal } from "react-terminal";
+import React, { useState } from "react";
 
 function Terminal({ setPage, commands, id }) {
+  const [selectedFile, setSelectedFile] = useState();
+  const [isFilePicked, setIsFilePicked] = useState(false);
+
+  const changeHandler = (event) => {
+    setSelectedFile(event.target.files[0]);
+    setIsFilePicked(true);
+  };
+
+  const handleSubmission = () => {
+    const formData = new FormData();
+
+    formData.append("File", selectedFile);
+
+    fetch(`http://${process.env.REACT_APP_IP}:8080/api/upload/:${id}`, {
+      method: "POST",
+      body: formData,
+    })
+      .then((response) => response.json())
+      .then((result) => {
+        console.log("Success:", result);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  };
 
   let content = (
-    <ReactTerminal 
-      commands={commands} 
-    />
+    <div>
+      <input type="file" name="file" onChange={changeHandler} />
+      {isFilePicked ? (
+        <div>
+          <p>Filename: {selectedFile.name}</p>
+          <p>
+            Size in bytes: {(selectedFile.size / Math.pow(1000, 2)).toFixed(2)}{" "}
+            mb
+          </p>
+          <p>
+            lastModifiedDate:{" "}
+            {selectedFile.lastModifiedDate.toLocaleDateString()}
+          </p>
+        </div>
+      ) : (
+        <p>Select a file to show details</p>
+      )}
+      <div>
+        <button onClick={handleSubmission}>Submit</button>
+      </div>
+      <ReactTerminal commands={commands} />
+    </div>
   );
   return content;
 }
