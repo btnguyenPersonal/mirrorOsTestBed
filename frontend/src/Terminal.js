f98c314152d836b567cb1b9b59b1b96489a561b0
Christopher Spartz <cgspz.17@gmail.com>
2022-03-23 10:24:25 -0500

    Fixed some errors and added resubmit button

--- frontend/src/Terminal.js
+++ frontend/src/Terminal.js
@@ -23,49 +23,60 @@ function Terminal({ setPage, commands, id }) {
       .then((response) => response.json())
       .then((result) => {
         //console.log("Success:", result);
-        setFileUploaded(true);
-        if(result.status!=200){
-          document.getElementById("fileStatus").innerHTML="<p>"+result.message+"</p>";
-        }else{
-          document.getElementById("fileStatus").innerHTML="<p>"+result.message+"</p>";
+        if (result.message != "Successful file upload") {
+          document.getElementById("errorStatus").innerHTML =
+            "<p>" + result.message + "</p>";
+        } else {
+          setFileUploaded(true);
+          document.getElementById("fileStatus").innerHTML =
+            "<p>" + result.message + "</p>";
         }
       })
       .catch((error) => {
         //console.error("Error:", error);
         setFileUploaded(true);
-        document.getElementById("fileStatus").innerHTML="<p>"+error+"</p>";
+        document.getElementById("fileStatus").innerHTML =
+          "<p>" + error + "</p>";
       });
   };
 
   const Upload = () => {
     return (
       <div>
-      <input type="file" name="file" onChange={changeHandler} />
-      {isFilePicked ? (
+        <input type="file" name="file" onChange={changeHandler} />
+        {isFilePicked ? (
+          <div>
+            <p>Filename: {selectedFile.name}</p>
+            <p>
+              Size in bytes:{" "}
+              {(selectedFile.size / Math.pow(1000, 2)).toFixed(2)} mb
+            </p>
+            <p>
+              lastModifiedDate:{" "}
+              {selectedFile.lastModifiedDate.toLocaleDateString()}
+            </p>
+          </div>
+        ) : (
+          <p>Select a file to show details</p>
+        )}
         <div>
-          <p>Filename: {selectedFile.name}</p>
-          <p>
-            Size in bytes: {(selectedFile.size / Math.pow(1000, 2)).toFixed(2)}{" "}
-            mb
-          </p>
-          <p>
-            lastModifiedDate:{" "}
-            {selectedFile.lastModifiedDate.toLocaleDateString()}
-          </p>
+          <button onClick={handleSubmission}>Submit</button>
         </div>
-      ) : (
-        <p>Select a file to show details</p>
-      )}
-      <div>
-        <button onClick={handleSubmission}>Submit</button>
-      </div>
+        <div id="errorStatus"></div>
       </div>
     );
-  }
+  };
 
   let content = (
     <div>
-      {fileUploaded ? <div id="fileStatus"></div> : <Upload />}
+      {fileUploaded ? (
+        <div>
+          <div id="fileStatus"></div> <br />{" "}
+          <button onClick={() => setFileUploaded(false)}>Submit new File</button>{" "}
+        </div>
+      ) : (
+        <Upload />
+      )}
       <ReactTerminal commands={commands} />
     </div>
   );
