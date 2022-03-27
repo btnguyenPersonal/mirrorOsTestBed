5390cfeef1d7572e667589590ebd4ffd8d0262c1
Ghoulshine <btbaccam@iastate.edu>
2022-03-27 11:24:25 -0500

    Added WebSocket functions to the Terminal.js file; successfully tested local server WebSocket handshake with Xterm-for-react interaction (console messages & user input sends from frontend and is received/echoed from the backend). Terminal cursor still does not perform backspace/delete key functionality...bugs still occur with arrow key movement and new input (overwriting preivous terminal output).

--- frontend/src/terminalComponents/Terminal.js
+++ frontend/src/terminalComponents/Terminal.js
@@ -2,52 +2,71 @@ import React, { useEffect } from 'react'
 import { XTerm } from 'xterm-for-react'
  
 function Terminal() {
-    const XTermRef = React.useRef();
-    const XTermOpt = {
-        cursorBlink:true
+  const XTermRef = React.useRef();
+  const XTermOpt = {
+    cursorBlink:true
+  }
+
+  let messageString = "";
+
+  const prompt = (firstLine) => {
+    if(firstLine) {
+      XTermRef.current.terminal.write('$ ');
+    }else{
+      XTermRef.current.terminal.write('\r\n$ ');
+    }
+  };
+
+  const onKey = (event) => {
+    if(event.key === '\r') {
+      //send through websocket
+      console.log("sending msg...");
+      ws.send(messageString);
+      messageString = "";
+      prompt(false);
     }
 
-    let messageString = "";
+    else {
+      console.log(event.key);
+      messageString += event.key;
+      XTermRef.current.terminal.write(event.key);
+    }
+  };
 
-    const prompt = (firstLine) => {
-        if(firstLine)
-            XTermRef.current.terminal.write('$ ');
-        else
-            XTermRef.current.terminal.write('\r\n$ ');
-    };
+  const ws = new WebSocket("ws://localhost:9000");
+  const msg = "frontend WebSocket is now open"
 
-    const onKey = (event) => {
-        if(event.key === '\r') {
-            prompt(false);
-            //send through websocket
-            messageString = "";
-        }
-
-        else {
-            console.log(event.key);
-            messageString += event.key;
-            XTermRef.current.terminal.write(event.key);
-        }
-    };
-
-    React.useEffect(() => {
-        //initialize websocket
-        prompt(true);
+  React.useEffect(() => {
+      //initialize websocket
+    ws.addEventListener("open", () =>{
+      console.log("connected to server");
+      ws.send(msg);
     });
+    prompt(true);
+  });
 
     //websocket on message
+    ws.onmessage = function (event) {
+      console.log(event.data);
+      XTermRef.current.terminal.write(event.data);
+      prompt(false);
+    };
 
-    let content = (
-        <div>
-            <XTerm
-                ref = { XTermRef }
-                options = { XTermOpt }
-                onKey = { onKey } 
-            />
-        </div>
-    );
+    //websocket for closing
+//        ws.addEventListener("close", function () {
+//          console.log("closing");
+//          prompt(false);
+//        });
 
-    return content;
+  let content = (
+    <div>
+      <XTerm
+        ref = { XTermRef }
+        options = { XTermOpt }
+        onKey = { onKey }
+      />
+    </div>
+  );
+  return content;
 }
-
 export default Terminal;
